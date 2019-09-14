dd32db0c7b3dd0236972c045e3d43731e0678025
Matej <ematej.snuderl@gmail.com>
2019-09-14 15:25:36 +0200

    dont create object for every exception

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -3,13 +3,15 @@ import * as Sentry from '@sentry/browser';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Extras = { [key: string]: any };
 
+const NO_EXTRAS: Extras = {};
+
 export default class SentryUtils {
   public static captureException = (
     error: Error | string,
-    extras: Extras = {},
+    extras: Extras = NO_EXTRAS,
   ): Promise<string> => {
     const exception = typeof error === 'string' ? new Error(error) : error;
-    console.error(exception);
+    console.error(exception, extras);
 
     return new Promise<string>(resolve => {
       Sentry.withScope(scope => {
