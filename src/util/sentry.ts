19452b5bdca1c6675b67f44a8a65e78c0a82fd77
Matej <ematej.snuderl@gmail.com>
2019-09-14 15:20:20 +0200

    support adding extras to Sentry scope

--- src/util/sentry.ts
+++ src/util/sentry.ts
@@ -1,9 +1,22 @@
 import * as Sentry from '@sentry/browser';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Extras = { [key: string]: any };
+
 export default class SentryUtils {
-  public static captureException = (error: Error | string): string => {
+  public static captureException = (
+    error: Error | string,
+    extras: Extras = {},
+  ): Promise<string> => {
     const exception = typeof error === 'string' ? new Error(error) : error;
     console.error(exception);
-    return Sentry.captureException(exception);
+
+    return new Promise<string>(resolve => {
+      Sentry.withScope(scope => {
+        scope.setExtras(extras);
+        const errorId = Sentry.captureException(exception);
+        resolve(errorId);
+      });
+    });
   };
 }
