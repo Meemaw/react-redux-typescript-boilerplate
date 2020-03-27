5bd727a2168b3e56a4647898eda003bd92994669
Meemaw <Meemaw@users.noreply.github.com>
2020-03-27 06:44:44 +0000

    Automated deployment: Fri Mar 27 06:44:44 UTC 2020 e064cfb2c9c404ba7f73a7499ca3a15e8db0283a

---
+++ precache-manifest.7af2e16241cc82831b20f8c93e92f8c3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe78cc8077187f253c2d7b802644d49",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.24f23be55720907ec24d.bundle.js"
+  },
+  {
+    "url": "runtime~main.24f23be55720907ec24d.bundle.js"
+  },
+  {
+    "url": "vendors~main.24f23be55720907ec24d.bundle.js"
+  },
+  {
+    "url": "vendors~main.24f23be55720907ec24d.bundle.js.LICENSE"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.03bda4375fc7b4ecf24319b260b287e4.js"
+  "precache-manifest.7af2e16241cc82831b20f8c93e92f8c3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
